25ad21b8794bd326aab9c4ce24c0023ef6dfa842
Becky McClain <beckykelley8@yahoo.com>
2018-05-01 20:38:55 -0700

    modal pops up

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,9 +6,9 @@ app.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname + '../../public/survey.html'));
 });
 
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname + '../../public/home.html'));
-// });
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '../../public/home.html'));
+});
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../public/home.html'))
