de085e6ecf6991c84e39961918a1c4d08c4fbef4
Becky McClain <beckykelley8@yahoo.com>
2018-05-01 22:46:01 -0700

    modal and  styling work

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,8 +10,8 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '../../public/home.html'));
 });
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../public/home.html'))
-});
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../../public/home.html'))
+// });
 
 };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/app/public"));
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
