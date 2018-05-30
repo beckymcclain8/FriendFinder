c0c86bbe48f6ab82c3bfd247c21900219cb5c190
Becky McClain <beckykelley8@yahoo.com>
2018-05-30 13:52:15 -0700

    update links

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + "/app/public"));
+// app.use(express.static("public"));
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routing/htmlRoutes")(app); 
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
